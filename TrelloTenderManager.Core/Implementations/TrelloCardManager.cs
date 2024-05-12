acf96ea661966225156d5cacad7ac39ea7424c77
Teshvier Mathura <teshvier.mathura@datasnipper.com>
2024-05-12 12:09:34 +0200

    Simplify NotImplementedException calls in TrelloCardManager
    The `System.NotImplementedException()` calls in the `Create`, `Update`, and `Exists` methods of the `TrelloCardManager` class have been simplified to `NotImplementedException()`. This change is possible because the `System` namespace is likely already in use or imported in the file, making the explicit `System.` prefix unnecessary.

--- TrelloTenderManager.Core/Implementations/TrelloCardManager.cs
+++ TrelloTenderManager.Core/Implementations/TrelloCardManager.cs
@@ -7,16 +7,16 @@ public class TrelloCardManager : ITrelloCardManager
 {
     public void Create(Tender tender)
     {
-        throw new System.NotImplementedException();
+        throw new NotImplementedException();
     }
 
     public void Update(Tender tender)
     {
-        throw new System.NotImplementedException();
+        throw new NotImplementedException();
     }
 
     public bool Exists(Tender tender)
     {
-        throw new System.NotImplementedException();
+        throw new NotImplementedException();
     }
 }
