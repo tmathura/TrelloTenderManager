4df76b32408ff77341814c1c1f7981cf674c751f
Teshvier Mathura <teshvier.mathura@datasnipper.com>
2024-05-21 02:41:22 +0200

    Refactor code to handle exceptions with try-catch block
    Refactored the main execution of the program to be wrapped in a try-catch block for better exception handling and logging using `log4net`. The logger initialization, service additions to the `builder` object, `HttpResponseExceptionFilter` addition, routing options, `log4net` configuration, `app` object building, service fetching, Swagger UI usage in development, HTTPS redirection, authorization, controller mapping, and application running are now all performed inside the try block. In the catch block, exception details are logged and rethrown.

--- TrelloTenderManager.WebApi/Program.cs
+++ TrelloTenderManager.WebApi/Program.cs
@@ -1,3 +1,4 @@
+using log4net;
 using System.Reflection;
 using TrelloTenderManager.Core.Implementations;
 using TrelloTenderManager.Core.Interfaces;
@@ -10,59 +11,69 @@ namespace TrelloTenderManager.WebApi;
 
 public class Program
 {
+    private static readonly ILog Logger = LogManager.GetLogger(MethodBase.GetCurrentMethod()?.DeclaringType);
+
     /// <summary>
     /// The entry point of the application.
     /// </summary>
     /// <param name="args">The command-line arguments.</param>
     public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
-
-        builder.Services.AddControllers();
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        builder.Services.AddSingleton<ICsvQueueDal, CsvQueueDal>();
-        builder.Services.AddSingleton<ICsvQueueBl, CsvQueueBl>();
-        builder.Services.AddSingleton<ICsvHelperWrapper, CsvHelperWrapper>();
-        builder.Services.AddSingleton<ITenderCsvParser, TenderCsvParser>();
-        builder.Services.AddSingleton<ITrelloDotNetWrapper, TrelloDotNetWrapper>();
-        builder.Services.AddSingleton<IListOnBoardManager, ListOnBoardManager>();
-        builder.Services.AddSingleton<ICustomFieldOnBoardManager, CustomFieldOnBoardManager>();
-        builder.Services.AddSingleton<ICustomFieldManager, CustomFieldManager>();
-        builder.Services.AddSingleton<IBoardManager, BoardManager>();
-        builder.Services.AddSingleton<ICardManager, CardManager>();
-
-        builder.Services.AddHostedService<QueueService>();
-
-        builder.Services.AddControllers(options =>
+        try
         {
-            options.Filters.Add<HttpResponseExceptionFilter>();
-        });
+            var builder = WebApplication.CreateBuilder(args);
 
-        builder.Services.AddRouting(routeOptions => routeOptions.LowercaseUrls = true);
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
-        var repository = log4net.LogManager.GetRepository(Assembly.GetEntryAssembly());
-        var fileInfo = new FileInfo("log4net.config");
-        log4net.Config.XmlConfigurator.Configure(repository, fileInfo);
+            builder.Services.AddSingleton<ICsvQueueDal, CsvQueueDal>();
+            builder.Services.AddSingleton<ICsvQueueBl, CsvQueueBl>();
+            builder.Services.AddSingleton<ICsvHelperWrapper, CsvHelperWrapper>();
+            builder.Services.AddSingleton<ITenderCsvParser, TenderCsvParser>();
+            builder.Services.AddSingleton<ITrelloDotNetWrapper, TrelloDotNetWrapper>();
+            builder.Services.AddSingleton<IListOnBoardManager, ListOnBoardManager>();
+            builder.Services.AddSingleton<ICustomFieldOnBoardManager, CustomFieldOnBoardManager>();
+            builder.Services.AddSingleton<ICustomFieldManager, CustomFieldManager>();
+            builder.Services.AddSingleton<IBoardManager, BoardManager>();
+            builder.Services.AddSingleton<ICardManager, CardManager>();
 
-        var app = builder.Build();
+            builder.Services.AddHostedService<QueueService>();
 
-        app.Services.GetRequiredService<ICsvQueueDal>();
-        app.Services.GetRequiredService<IBoardManager>();
+            builder.Services.AddControllers(options =>
+            {
+                options.Filters.Add<HttpResponseExceptionFilter>();
+            });
 
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            builder.Services.AddRouting(routeOptions => routeOptions.LowercaseUrls = true);
+
+            var repository = log4net.LogManager.GetRepository(Assembly.GetEntryAssembly());
+            var fileInfo = new FileInfo("log4net.config");
+            log4net.Config.XmlConfigurator.Configure(repository, fileInfo);
+
+            var app = builder.Build();
+
+            app.Services.GetRequiredService<ICsvQueueDal>();
+            app.Services.GetRequiredService<IBoardManager>();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
         }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        app.Run();
+        catch (Exception exception)
+        {
+            Logger.Error($"{exception.Message} - {exception.StackTrace}");
+            throw;
+        }
     }
 }
